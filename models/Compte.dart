1bdc6b54f9e7a92e5ee86bcead19a17a0c9e98ac
Frankstein AFOMALE <afomaledrank@gmail.com>
2023-04-21 21:58:28 +0000

    My first commit

---
+++ models/Compte.dart
@@ -0,0 +1,43 @@
+import 'client.dart';
+
+enum TypeCompte { EPARGNE, COURANT }
+
+class Compte {
+  String? numCpt;
+  static int compteur = 0;
+  double solde = 0;
+  DateTime? dateCrea;
+  TypeCompte type;
+  Client client;
+
+  Compte(this.client, this.type) {
+    this.numCpt;
+    if (compteur < 1000) {
+      compteur++;
+      numCpt = "KOFBANK-" +
+          this.client.nom.substring(0, 1) +
+          this.client.prenom.substring(0, 1) +
+          "-$compteur";
+      this.dateCrea = DateTime.now();
+      this.solde = 0;
+    }
+  }
+
+  faireDepot(double montant) {
+    this.solde += montant;
+  }
+
+  faireRetrait(double montant) {
+    if (this.solde >= montant) {
+      this.solde -= montant;
+      return true;
+    }
+    return false;
+  }
+
+  faireTransfert(Compte dest, double montant) {
+    if (this.faireRetrait(montant)) {
+      dest.faireDepot(montant);
+    }
+  }
+}
